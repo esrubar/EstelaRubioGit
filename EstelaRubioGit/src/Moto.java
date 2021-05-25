0e1532528f4716b72611e53756dbec77e89e5a2d
windows10 <windows10@DESKTOP-B8E4IF5>
2021-05-25 08:14:24 +0200

    gfsr

--- EstelaRubioGit/src/Moto.java
+++ EstelaRubioGit/src/Moto.java
@@ -6,9 +6,4 @@ public class Moto extends Vehiculo {
 		super (0,2);
 		llevoRadio = false;
 	}
-	
-	public void showInfo() {
-		System.out.println("Soy un vheiculo");
-	}
-	
 }
--- EstelaRubioGit/src/Vehiculo.java
+++ EstelaRubioGit/src/Vehiculo.java
@@ -3,8 +3,9 @@ public class Vehiculo {
 	private int numPuertas;
 	private int numRuedas;
 
-	public Vehiculo(int numPuertas, int numRuedas) {
+	public Vehiculo(int numRuedas, int numPuertas) {
 		this.numPuertas = numPuertas;
 		this.numRuedas = numRuedas;
+
 	}
 }
