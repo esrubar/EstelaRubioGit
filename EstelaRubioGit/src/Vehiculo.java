50db31055bd696a2e223079baedd3d95f120384a
windows10 <windows10@DESKTOP-B8E4IF5>
2021-05-25 07:27:59 +0200

    Clase sobre vheiculos

---
+++ EstelaRubioGit/src/Vehiculo.java
@@ -0,0 +1,10 @@
+
+public class Vehiculo {
+	private int numPuertas;
+	private int numRuedas;
+
+	public Vehiculo(int numPuertas, int numRuedas) {
+		this.numPuertas = numPuertas;
+		this.numRuedas = numRuedas;
+	}
+}
